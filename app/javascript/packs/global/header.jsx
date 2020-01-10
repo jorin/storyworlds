185d975367b0cb00c79f2b917ea5fd064a878dce
Jorin Garguilo <joring@gmail.com>
2020-01-09 22:31:48 -0600

    explicitly add stylesheet pack tags to views

--- app/javascript/packs/global/header.jsx
+++ app/javascript/packs/global/header.jsx
@@ -1,6 +1,5 @@
 import RenderService from 'services/render_service';
 import Header from 'components/global/header';
-import 'styles/global/header';
 
 document.addEventListener('DOMContentLoaded', () => {
   RenderService.render('header', Header);
