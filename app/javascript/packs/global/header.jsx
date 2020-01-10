64f17a8f0e35d37591b8cebfb05b1653b118f3e6
Jorin Garguilo <joring@gmail.com>
2020-01-09 22:00:05 -0600

    include stylesheets for header

--- app/javascript/packs/global/header.jsx
+++ app/javascript/packs/global/header.jsx
@@ -1,5 +1,6 @@
 import RenderService from 'services/render_service';
 import Header from 'components/global/header';
+import 'styles/global/header';
 
 document.addEventListener('DOMContentLoaded', () => {
   RenderService.render('header', Header);
